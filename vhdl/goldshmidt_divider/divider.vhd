e0fa49f11bb6f6333174bf22d8cd53cb95ebcc4c
gzf1992 <gzf1992@gmail.com>
2014-06-02 18:24:47 +0100

    working for some denormals, still hv some problem with precision

--- vhdl/goldshmidt_divider/divider.vhd
+++ vhdl/goldshmidt_divider/divider.vhd
@@ -23,26 +23,26 @@ ARCHITECTURE rtl of div is
   type rom is array (0 to 15) of std_logic_vector(26 downto 0);
   type intermediate is array (0 to lsize) of std_logic_vector(26 downto 0);  -- intermediate signals
 
-    
+  --input signals
   SIGNAL A_si_s ,B_si_s  : std_logic;
-  SIGNAL A_e_s,B_e_s 	 : std_logic_vector(7 downto 0);
+  SIGNAL A_e_s,B_e_s 	   : std_logic_vector(7 downto 0);
   SIGNAL A_significand_s,B_significand_s,sft_B_significand_s: std_logic_vector(26 downto 0);
-
+  --rom index
   signal selection       : integer range 0 to 15;          -- select rom content
-
+  --intermediate signals during iterations
+  SIGNAL x_0		           : slv(26 downto 0);
   SIGNAL n,d,f           : intermediate;
-  
-  SIGNAL x_0		 : slv(26 downto 0);
-  SIGNAL leadingzeros: integer range 0 to 27;
+  --LZC signals
+  SIGNAL leadingzeros1,leadingzeros2: integer range 0 to 27;
+  --exception flags
   SIGNAL prenorm_result_exception:std_logic;
   SIGNAL div_opA_is_zero,div_opB_is_zero,div_opA_is_infinity,div_opB_is_infinity,div_opA_is_normal,div_opB_is_normal:std_logic;
-  
+  --normalization signals
   SIGNAL prenorm_e_s	 :slv(8 downto 0);
   SIGNAL prenorm_significand_s  : slv(26 downto 0);
-
   SIGNAL postnorm_e_s	 :slv(7 downto 0);
   SIGNAL postnorm_man_s	 :slv(24 downto 0);
-
+  --final output signa;s
   SIGNAL finalised_si_s  : std_logic;
   SIGNAL finalised_e_s   : std_logic_vector(7 downto 0);
   SIGNAL finalised_man_s : std_logic_vector(22 downto 0);
@@ -78,8 +78,8 @@ div_opB_is_infinity<='1' WHEN div_IN2(30 downto 0)="1111111100000000000000000000
 div_opA_is_normal<='0' WHEN usg(div_IN1(30 downto 23))=0 AND usg(div_IN1(22 DOWNTO 0))/=0 ELSE '1'; 
 div_opB_is_normal<='0' WHEN usg(div_IN2(30 downto 23))=0 AND usg(div_IN2(22 DOWNTO 0))/=0 ELSE '1'; 
 
-
 prenorm_result_exception<='1' WHEN prenorm_e_s(8)='1' OR usg(prenorm_e_s)=0 ELSE '0';
+
 ------------------------------------------------------
 --unpack the inputs
 ------------------------------------------------------
@@ -87,51 +87,79 @@ unpack:BLOCK
   BEGIN
     A_si_s<=div_IN1(31);
     A_e_s<=div_IN1(30 downto 23);
-    A_significand_s<=div_opA_is_normal&div_IN1(22 downto 0)&"000";  --modified to 27 bits(Hidden+man+3bit guard)
+    A_significand_s<=div_opA_is_normal&div_IN1(22 downto 0)&"000";  --normalized to 27 bits(restore Hidden + man +3 guard-bits)
       
     B_si_s<=div_IN2(31);
     B_e_s<=div_IN2(30 downto 23);
     B_significand_s<=div_opb_is_normal&div_IN2(22 downto 0)&"000";
 
---    selection<=to_integer(usg(div_IN2(22 downto 19))); --choose initial guess 
   END BLOCK unpack;
-
---------------------------------------------------------------------------------------
---leading zero detector
---The process detect the number of leading zeros in result to enable
---normalization in later stage
---------------------------------------------------------------------------------------
-
-leading_zero_detector:PROCESS(B_significand_s)
-
-VARIABLE count		:integer range 0 to 27;
-VARIABLE sft_B  :usg(26 downto 0);
-BEGIN
-  count:=0;
-  
-sft_B:=usg(B_significand_s);
-	FOR i IN B_significand_s'HIGH DOWNTO  B_significand_s'LOW LOOP
-		IF B_significand_s(i)='0' 	THEN
-		  count:=count+1;
-		  sft_B:=sft_B sll 1;
-		ELSE EXIT;
-		END IF;
-	END LOOP;
-	sft_B_significand_s<=slv(sft_B);
-	selection<=to_integer(sft_B(25 downto 22));          --choose initial guess 
-	leadingzeros	<= count;
-END PROCESS leading_zero_detector;
 ------------------------------------------------------
 --pack outputs
 ------------------------------------------------------
-div_OUT(31)<=finalised_si_s;
-div_OUT(30 downto 23)<=finalised_e_s;
-div_OUT(22 downto 0)<=finalised_man_s;
+pack:BLOCK
+  BEGIN
+    div_OUT(31)<=finalised_si_s;
+    div_OUT(30 downto 23)<=finalised_e_s;
+    div_OUT(22 downto 0)<=finalised_man_s;
+  END BLOCK pack;
+------------------------------------------------------------------------------------------------
+--leading zero counter
+--The process count the number of leading zeros in dividend. 
+--It shift the denominator to obtain the form of 1.xxxxx and compute which initial guess to take
+------------------------------------------------------------------------------------------------
 
-x_0<=lut(selection);
+LZC1:PROCESS(B_significand_s)
+VARIABLE count		:integer range 0 to 27;
+VARIABLE sft_B  :usg(26 downto 0);
+
+BEGIN
+  count:=0;  
+  sft_B:=usg(B_significand_s);
+
+	FOR i IN B_significand_s'HIGH DOWNTO  B_significand_s'LOW LOOP
+		IF B_significand_s(i)='0' 	THEN
+		  count:=count+1;                                   --increment count
+		  sft_B:=sft_B sll 1;                               --shift left by one bit
+		ELSE EXIT;
+		END IF;
+	END LOOP;
+
+	sft_B_significand_s<=slv(sft_B);
+	selection<=to_integer(sft_B(25 downto 22));           --initial guess in the form of 1.xxxx 
+	leadingzeros1	<= count;                                --stores zero count
+END PROCESS LZC1;
+
+-----------------------------------------------------------------
+--exponent logic
+--This process handles exponent field variation due to denormals
+--restore exponent because of zero shifting of B significand
+-----------------------------------------------------------------
+
+expo_logic:PROCESS (div_opA_is_normal,div_opB_is_normal,leadingzeros1,A_e_s,B_e_s)
+variable mode: slv(1 downto 0);
+BEGIN
+    mode:=div_opA_is_normal&div_opB_is_normal;
+  CASE mode IS
+    WHEN  "00"=>prenorm_e_s  <=slv(to_unsigned (126+leadingzeros1,9));
+    WHEN  "01"=>prenorm_e_s  <=slv(RESIZE(usg(A_e_s),9)-RESIZE(usg(B_e_s),9)+127);
+    WHEN  "10"=>prenorm_e_s  <=slv(RESIZE(usg(A_e_s),9)-RESIZE(usg(B_e_s),9)+128+leadingzeros1);
+    WHEN  "11"=>prenorm_e_s		<=slv(RESIZE(usg(A_e_s),9)-RESIZE(usg(B_e_s),9)+127);
+    WHEN OTHERS=>NULL;
+  END CASE;
+
+END PROCESS expo_logic;
+
+
+
+-----------------------------------------------------------------------------------------------------
+--iterative method
+-----------------------------------------------------------------------------------------------------
+
+x_0<=lut(selection);                                    --read initial guess from rom  
 
 ------------------------------------------------------
---significand division (range of result: 0.5<x<2)
+--significand division 
 --    	   N f(0) f(1) f(2) f(3)
 --compute -*----*----*----*---- such that the dividend 
 --        D f(0) f(1) f(2) f(3)  
@@ -139,8 +167,8 @@ x_0<=lut(selection);
 ------------------------------------------------------
 div_gen:FOR i in 0 to lsize GENERATE	
 	case0: IF i=0 GENERATE                                 -- first iteration
-    		mult0:ENTITY mult27bit 	-- d(0)=x_0*b              -- multiplied by a initial guess of 
-		PORT MAP (                                           -- 1/b to try to set dividend to 1  
+    		mult0:ENTITY mult27bit 	-- d(0)=x_0*b              -- multiplied by a initial guess of 1/b 
+		PORT MAP (                                           
    		    A_in  => x_0,                                   
         	B_in  => sft_B_significand_s,
         	C_out => d(0)
@@ -148,7 +176,7 @@ div_gen:FOR i in 0 to lsize GENERATE
 
     		f(0)<=NOT d(0); 	                                  -- f=1-d(0)                      
 
-	   	mult1:ENTITY mult27bit 	-- n(0)=a*x_0              -- multiply with initial guess accordingly
+	   	mult1:ENTITY mult27bit 	-- n(0)=a*x_0              -- multiply with initial guess
 		PORT MAP (
         	A_in  => x_0,
         	B_in  => A_significand_s,
@@ -173,45 +201,82 @@ div_gen:FOR i in 0 to lsize GENERATE
 	END GENERATE case1;
 END GENERATE div_gen;
 
-------------------------------------------------------
---ready for normalization
-------------------------------------------------------
 prenorm_significand_s	<=n(lsize);	--use the final result as output
-prenorm_e_s		<=slv(RESIZE(usg(A_e_s),9)-RESIZE(usg(B_e_s),9)+127);
 
+
+
+-----------------------------------------------------------------------------------------------------
+--normalization stage
+-----------------------------------------------------------------------------------------------------
+LZC2:PROCESS(prenorm_significand_s)
+VARIABLE count		:integer range 0 to 27;
+
+BEGIN
+  count:=0;
+  FOR i IN prenorm_significand_s'HIGH DOWNTO prenorm_significand_s'LOW LOOP
+		IF prenorm_significand_s(i)='0' 	THEN                             
+		  count:=count+1;
+		ELSE EXIT;
+		END IF;	 
+	  END LOOP;
+	  
+	  leadingzeros2<=count;
+	  
+END PROCESS LZC2;
 
 ------------------------------------------------------
 --normalization
 ------------------------------------------------------
-normalise:PROCESS (prenorm_e_s,prenorm_significand_s,prenorm_result_exception,A_e_s)
---VARIABLE sticky_b :std_logic;
---VARIABLE sft_man  :slv(24 downto 0);
-VARIABLE sft_unit   :integer range 0 to 255;
+normalise:PROCESS (prenorm_e_s,prenorm_significand_s,prenorm_result_exception,A_e_s,leadingzeros2)
+VARIABLE sft_unit   :integer range 0 to 255 :=0;
+
 BEGIN
-  sft_unit:=to_integer(usg(NOT prenorm_e_s(7 downto 0))+1);--TO_INTEGER(abs(sgn(prenorm_e_s)));
-  IF prenorm_result_exception='1' AND A_e_s(7)='0' THEN
-    postnorm_e_s<=(OTHERS=>'0');
+  
+                                                                    
+  IF prenorm_result_exception='1' AND A_e_s(7)='0' THEN           --IF result causes a underflow
+    
+    sft_unit:=to_integer(usg(NOT prenorm_e_s(7 downto 0))+1);       --shifting prenorm significand by abs(prenorm_e_s) in case of underflow 
+                                                                  --will set the prenorm_e_s back to zero
+                                                                                                                                  
+    postnorm_e_s<=(OTHERS=>'0');                                  --denormal or zero
     FOR i in 0 to 24 LOOP
-      --IF i<abs(sgn(prenorm_e_s))+2 THEN
-       -- sticky_b:=sticky_b&prenorm_significand_s(i)
+      
       IF i+sft_unit<25 THEN
-        postnorm_man_s(i)<=prenorm_significand_s(i+sft_unit+2);
+        postnorm_man_s(i)<=prenorm_significand_s(i+sft_unit+2);   --shifting
       ELSE
-        postnorm_man_s(i)<='0';
+        postnorm_man_s(i)<='0';                                   --zero padding
       END IF;
       
     END LOOP;
-ELSE
-	   IF prenorm_significand_s(26)='1' THEN 
-		    postnorm_e_s<=prenorm_e_s(7 downto 0);
-		    postnorm_man_s(24 downto 1)<=prenorm_significand_s(25 downto 2);
-		    postnorm_man_s(0)<=prenorm_significand_s(1) OR prenorm_significand_s(0);
-	   ELSE
-		    postnorm_e_s<=slv(usg(prenorm_e_s(7 downto 0))-1);
-		    postnorm_man_s<=prenorm_significand_s(24 downto 0);
-	   END IF;
+    
+    
+    
+  ELSE                                                            --normal operation(overflow handled explicitly in rounding)
+  
+    
+    
+    FOR i in 0 to 24 LOOP    
+      
+      IF  i<leadingzeros2-1 THEN
+        postnorm_man_s(i)<='0';
+      ELSE
+        postnorm_man_s(i)<=prenorm_significand_s(i-leadingzeros2+1);
+      END IF;
+      
+    END LOOP;
+    postnorm_e_s<=slv(usg(prenorm_e_s(7 downto 0))-leadingzeros2+1);
+    
+    
+--	   IF prenorm_significand_s(26)='1' THEN 
+--		    postnorm_e_s<=prenorm_e_s(7 downto 0);
+--		    postnorm_man_s(24 downto 1)<=prenorm_significand_s(25 downto 2);
+--		    postnorm_man_s(0)<=prenorm_significand_s(1) OR prenorm_significand_s(0);
+--	   ELSE
+--		    postnorm_e_s<=slv(usg(prenorm_e_s(7 downto 0))-1);
+--		    postnorm_man_s<=prenorm_significand_s(24 downto 0);
+--	   END IF;
+--  END IF;
 END IF;
-
 END PROCESS normalise;
  
 ------------------------------------------------------
