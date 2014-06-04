a7b10f2a55178cb8df448bb141ce79215fd6df9e
gzf1992 <gzf1992@gmail.com>
2014-06-04 17:50:26 +0100

    added addition module

--- vhdl/multacc/multacc.vhd
+++ vhdl/multacc/multacc.vhd
@@ -48,6 +48,13 @@ architecture fused of multacc is
   signal post_mult_sign : std_logic;
   signal post_mult_exp : unsigned(8 downto 0);
   signal post_mult_significand : unsigned(47 downto 0); --with 2 integer bits
+
+  signal expo_diff : sgn(9 downto 0);
+  signal aligned_c : usg(71 downto 0);
+  signal post_add_lsresult : usg(24 downto 0);  --left shift result
+  signal post_add_rsresult : usg(47 downto 0);  --right shift result
+  signal a,b,c : float32_t;
+  signal pre_norm_signifcand : usg(47 downto 0);
 begin
   -----------------------------------------------------------------
   -- multiply
@@ -92,6 +99,93 @@ begin
     end if;
     post_mult_significand <= sig_a * sig_b;
   end process multiply;
+  -----------------------------------------------------------------------------
+  --This stage performs the pre-alignment of the adder input
+  -----------------------------------------------------------------------------
+  --unpack
+  a<=slv2float(multacc_in1);
+  b<=slv2float(multacc_in2);
+  c<=slv2float(multacc_in3);
+  
+  exp_diff<=sgn(Resize(a.exp,9)+Resize(b.exp,9)-Resize(c.exp,9));
+  --computes the exponent difference
+  
+  adder_c_align : process(multacc_in3,exp_diff)
+    variable add3 : float32_t;
+    variable sig_c: unsigned(23 downto 0);
+    variable shift_unit : integer range -256 to 255;
+  begin
+   add3:=slv2float(multacc_in3);
+   shift_unit:=to_integer(exp_diff);
+   
+   if add3.sign='1' then                --invert input if the sign is negative
+     if add3.exponent = zeros then
+      sig_c := not unsigned(add3.significand & '0')+1;  --normalise for
+                                                        --denormal and invert
+     else
+      sig_c := not unsigned('1' & add3.significand)+1;  --restore hidden bit
+                                                        --and invert
+     end if;
+   else
+     if add3.exponent = zeros then
+      sig_c := unsigned(add3.significand & '0');  --normalise for denormal
+     else
+      sig_c := unsigned('1' & add3.significand);  --restore hidden bit
+     end if;
+   end if;
+-------------------------------------------------------------------------------
+   --alignment shifting
+   --shift operand c to align with the multiplication result.
+   --shift_unit is a signed number which will shift the operand to the left or
+   --right:neg->shift left, pos->shift right
+   --                          xx.xxxxxxxxxxxxxxxxxxxxxxxx......
+   -- (neg shit by shift_unit<-)x.xxxxxxxxxxx....(->pos shift by shift_unit)
+-------------------------------------------------------------------------------
+     for i in 0 to 71 loop
+       if i+shift_unit<46 and i+shift_unit>21 then
+         aligned_c(i)<=sig_c(i-22+shift_unit);         
+       else
+         aligned_c(i)<='0';
+       end if;
+     end loop;  -- i				
+  end process adder_c_align;
+
+  -----------------------------------------------------------------------------
+  --48 bit adder and a multiplexer to compute the 72 bit result
+  -----------------------------------------------------------------------------
+  --The process calculates the sum of the aligned operands.
+  -----------------------------------------------------------------------------
+  adder:process(aligned_c,post_mult_significand)
+    variable result : unsigned(48 downto 0);
+  begin
+    result:=resize(post_mult_significand,49)+resize(unsigned(aligned_c(47 downto 0),49);
+
+    if result(48)='1' then
+      post_add_lsresult<=resize(aligned_c(71 downto 48),25)+1;
+    else
+      post_add_lsresult<=resize(aligned_c(71 downto 48),25);
+    end if;
     
+    post_add_rsresult<=result(47 downto 0);
+    
+  end process adder;
+  -----------------------------------------------------------------------------
+  --multiplexer to select actual significand bits
+  -----------------------------------------------------------------------------
+  mux:process(post_add_rsresult,post_add_lsresult)
+  begin
+    
+    if expo_diff>=0 then
+      
+    elsif expo_diff>-26 then
+            
+    else
+  
+    end if;
+
+
+
+    
+  end process mux;
   
 end architecture fused;
