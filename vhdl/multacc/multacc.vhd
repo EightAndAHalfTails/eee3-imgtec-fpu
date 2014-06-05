9521dadace70e500f8eac2e629e6d5a4de3ada74
gzf1992 <gzf1992@gmail.com>
2014-06-05 17:04:05 +0100

    updated multacc.vhd

--- vhdl/multacc/multacc.vhd
+++ vhdl/multacc/multacc.vhd
@@ -12,20 +12,20 @@ use work.types.all;
 ---------------------------------------------------
 
 entity multacc is
-  port(
+ port(
     multacc_in1, multacc_in2, multacc_in3 : in std_logic_vector(31 downto 0);
     multacc_out : out std_logic_vector(31 downto 0)
     );
 end entity multacc;
 
-architecture naive of multacc is
-  signal product : std_logic_vector(31 downto 0);  
-begin
-  multiplier: entity mult port map(
-    mult_in1 => multacc_in1,
-    mult_in2 => multacc_in2,
-    mult_out => product
-    );
+--architecture naive of multacc is
+ -- signal product : std_logic_vector(31 downto 0);  
+--begin
+ -- multiplier: entity mult port map(
+   -- mult_in1 => multacc_in1,
+  --  mult_in2 => multacc_in2,
+ --   mult_out => product
+--    );
 
   ------------------------------
   -- Placeholder adder.
@@ -35,14 +35,14 @@ begin
   -- modify adder to fit this
   -- specification
   ------------------------------
-  adder: entity addsub port map(
-    add_in1 => product,
-    add_in2 => multacc_in3,
-    add_out => multacc_out,
-    operation_i => '0'
-  );
+  --adder: entity addsub port map(
+  --  add_in1 => product,
+ --   add_in2 => multacc_in3,
+--    add_out => multacc_out,
+--    operation_i => '0'
+--  );
   ------------------------------
-end architecture naive;
+--end architecture naive;
 
 architecture fused of multacc is
   signal post_mult_sign : std_logic;
@@ -51,10 +51,15 @@ architecture fused of multacc is
 
   signal expo_diff : sgn(9 downto 0);
   signal aligned_c : usg(71 downto 0);
-  signal post_add_lsresult : usg(24 downto 0);  --left shift result
-  signal post_add_rsresult : usg(47 downto 0);  --right shift result
-  signal a,b,c : float32_t;
+  
+  signal a,b,c,result : float32_t;
+  
   signal pre_norm_signifcand : usg(47 downto 0);
+  signal pre_norm_exponent : usg(8 downto 0);
+  signal post_norm_significand : usg(25 downto 0);
+  signal post_norm_exponent : usg(7 downto 0);
+  signal rightshift : std_logic;
+  signal leadingzeros: integer range 0 to 48;
 begin
   -----------------------------------------------------------------
   -- multiply
@@ -99,93 +104,166 @@ begin
     end if;
     post_mult_significand <= sig_a * sig_b;
   end process multiply;
+
+
+-------------------------------------------------------------------------------
+  --pre-align stage
   -----------------------------------------------------------------------------
-  --This stage performs the pre-alignment of the adder input
+  --This stage performs the pre-alignment of the adder input with the
+  --multiplication result
+  -----------------------------------------------------------------------------
+  --Since the significand bits of c can be either shifted left or right based
+  --on the relative values of exponents. c is therefore extended to 72 bits  
   -----------------------------------------------------------------------------
   --unpack
   a<=slv2float(multacc_in1);
   b<=slv2float(multacc_in2);
   c<=slv2float(multacc_in3);
-  
-  exp_diff<=sgn(Resize(a.exp,9)+Resize(b.exp,9)-Resize(c.exp,9));
+  multacc_out<=float2slv(result);
+  expo_diff<=sgn(Resize(post_mult_exp,10)-Resize(usg(c.exponent),10));
   --computes the exponent difference
   
-  adder_c_align : process(multacc_in3,exp_diff)
-    variable add3 : float32_t;
+  adder_c_align : process(c,expo_diff)
+
     variable sig_c: unsigned(23 downto 0);
     variable shift_unit : integer range -256 to 255;
+    constant zeros : exponent_t := (others => '0');
   begin
-   add3:=slv2float(multacc_in3);
-   shift_unit:=to_integer(exp_diff);
+   shift_unit:=to_integer(expo_diff);
    
-   if add3.sign='1' then                --invert input if the sign is negative
-     if add3.exponent = zeros then
-      sig_c := not unsigned(add3.significand & '0')+1;  --normalise for
+   if c.sign='1' then                --invert input if the sign is negative
+     if c.exponent = zeros then
+      sig_c := not unsigned(c.significand & '0')+1;  --normalise for
                                                         --denormal and invert
      else
-      sig_c := not unsigned('1' & add3.significand)+1;  --restore hidden bit
+      sig_c := not unsigned('1' & c.significand)+1;  --restore hidden bit
                                                         --and invert
      end if;
    else
-     if add3.exponent = zeros then
-      sig_c := unsigned(add3.significand & '0');  --normalise for denormal
+     if c.exponent = zeros then
+      sig_c := unsigned(c.significand & '0');  --normalise for denormal
      else
-      sig_c := unsigned('1' & add3.significand);  --restore hidden bit
+      sig_c := unsigned('1' & c.significand);  --restore hidden bit
      end if;
    end if;
--------------------------------------------------------------------------------
+   --------------------------------------------------------------------------
    --alignment shifting
    --shift operand c to align with the multiplication result.
    --shift_unit is a signed number which will shift the operand to the left or
    --right:neg->shift left, pos->shift right
    --                          xx.xxxxxxxxxxxxxxxxxxxxxxxx......
    -- (neg shit by shift_unit<-)x.xxxxxxxxxxx....(->pos shift by shift_unit)
--------------------------------------------------------------------------------
+   --------------------------------------------------------------------------
+   if shift_unit<-25 then
+     aligned_c(71 downto 48)<=sig_c;
+     aligned_c(47 downto 0)<=(others=>'0');
+   else
      for i in 0 to 71 loop
        if i+shift_unit<46 and i+shift_unit>21 then
          aligned_c(i)<=sig_c(i-22+shift_unit);         
        else
          aligned_c(i)<='0';
        end if;
-     end loop;  -- i				
+     end loop;  -- i     
+   end if;
+   
   end process adder_c_align;
 
-  -----------------------------------------------------------------------------
-  --48 bit adder and a multiplexer to compute the 72 bit result
+  
+-------------------------------------------------------------------------------
+  --adder
   -----------------------------------------------------------------------------
   --The process calculates the sum of the aligned operands.
   -----------------------------------------------------------------------------
-  adder:process(aligned_c,post_mult_significand)
-    variable result : unsigned(48 downto 0);
-  begin
-    result:=resize(post_mult_significand,49)+resize(unsigned(aligned_c(47 downto 0),49);
+  --A 48 bit adder and a multiplexer are used to compute the sum of c and
+  --result of multiplication. Then the result is truncated to 48 bits based on
+  --where the valid bits are. 
+  -----------------------------------------------------------------------------
 
-    if result(48)='1' then
-      post_add_lsresult<=resize(aligned_c(71 downto 48),25)+1;
-    else
-      post_add_lsresult<=resize(aligned_c(71 downto 48),25);
-    end if;
+  adder:process(aligned_c,post_mult_significand,post_mult_exp)
+    variable result : unsigned(48 downto 0);
+    variable post_add_lsresult:unsigned(24 downto 0);
+    variable post_add_rsresult:unsigned(47 downto 0);
+  begin
     
-    post_add_rsresult<=result(47 downto 0);
+    result:=resize(post_mult_significand,49)+resize(usg(aligned_c(47 downto 0)),49);
+
+    if result(48)='1' then                                              --overflow
+      post_add_lsresult:=resize(aligned_c(71 downto 48),25)+1;
+    else
+      post_add_lsresult:=resize(aligned_c(71 downto 48),25);
+    end if;    
+    post_add_rsresult:=result(47 downto 0);
+    
+  ------------------------------------------------
+  --multiplexer to select actual significand bits
+  ------------------------------------------------
+    if post_add_lsresult=0 then
+      pre_norm_signifcand<=post_add_rsresult;
+      pre_norm_exponent<=post_mult_exp;
+      rightshift<='1';
+    else
+      pre_norm_signifcand<=post_add_lsresult(23 downto 0)&post_add_rsresult(47 downto 24);
+      pre_norm_exponent<=post_mult_exp+25;
+      rightshift<='0';
+    end if;
     
   end process adder;
-  -----------------------------------------------------------------------------
-  --multiplexer to select actual significand bits
-  -----------------------------------------------------------------------------
-  mux:process(post_add_rsresult,post_add_lsresult)
-  begin
-    
-    if expo_diff>=0 then
-      
-    elsif expo_diff>-26 then
-            
-    else
   
+-------------------------------------------------------------------------------  
+  --normalization and rounding
+  -----------------------------------------------------------------------------
+  normalise:process(expo_diff,pre_norm_signifcand,aligned_c,c,pre_norm_exponent)
+    variable leadingzeros : integer range 0 to 48;
+    variable sft_result_significand : usg(47 downto 0);
+  begin
+    leadingzeros := 0;
+    sft_result_significand:=pre_norm_signifcand;
+    for i in pre_norm_signifcand'high downto pre_norm_signifcand'low loop
+      if pre_norm_signifcand(i)='0' then
+        leadingzeros:=leadingzeros+1;
+        sft_result_significand:=sft_result_significand sll 1;
+      else
+        exit; 
+      end if;
+    end loop;  -- i
+
+    if expo_diff<-25 then
+      post_norm_significand<=aligned_c(71 downto 48)&"00";
+      post_norm_exponent<=usg(c.exponent);
+    else
+      post_norm_significand<=sft_result_significand(47 downto 22);
+      post_norm_exponent<=usg(pre_norm_exponent(7 downto 0))-leadingzeros;
     end if;
 
+  end process normalise;
 
+  --------------------------------------------------------------------------------------
+  --rounder
+  --The process round the result to be to be 23 bit mantissa
+  --------------------------------------------------------------------------------------
+  rounder:PROCESS(post_norm_significand,post_norm_exponent)
 
-    
-  end process mux;
+    VARIABLE rounded_result_e_s		:usg(7 downto 0);
+    VARIABLE rounded_result_man_s	:usg(23 downto 0);
+
+  BEGIN
+	
+    CASE post_norm_significand(2 downto 0) IS						--rounding decoder(LSB+GUARD+STICKY)  RTE rounding mode
+      WHEN "000"|"001"|"010"|"100"|"101"=>rounded_result_man_s := '0'&post_norm_significand(24 downto 2);			--round down
+      WHEN "011"|"110"|"111"		=>rounded_result_man_s := resize(usg(post_norm_significand(24 downto 2)),24)+1;	--round up
+      WHEN OTHERS => NULL;
+    END CASE;
   
+    IF rounded_result_man_s(23)='1' THEN
+      rounded_result_e_s:=usg(post_norm_exponent)+1;
+    ELSE
+      rounded_result_e_s		:=post_norm_exponent;
+    END IF;
+	
+    result.exponent	<=	slv(rounded_result_e_s);
+    result.significand	<=	slv(rounded_result_man_s(22 downto 0));	--1 bit shift adjustment
+    result.sign<='1';
+  END PROCESS rounder;
+
 end architecture fused;
